8f5fe49cda4c0e35627d81aa632c6434112f767e
Harsh Sharma <120568101+HarshSharmaIN@users.noreply.github.com>
2023-09-25 20:42:05 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,58 +4,72 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+mongoose.connect("mongodb+srv://Cluster74109:XEVdXU5sclpR@cluster74109.ue32tcy.mongodb.net/blogDB");
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+const homeStartingContent = "Welcome to Daily Journal - Your Source for Inspiration and Knowledge! Explore an array of captivating topics, from inspiring stories of resilience to cutting-edge science and technology. Discover wellness insights, travel adventures, and creative inspirations that enrich your life. Join our vibrant community, share your thoughts, and stay updated with our newsletter. Let's embark on this journey together, embracing the beauty of diversity and fostering personal growth. Spark your curiosity, ignite your creativity, and find fulfillment in our thought-provoking content. Thank you for being a part of our quest to inspire and enlighten. Happy reading!";
+const aboutContent = "Unlock the World of Knowledge and Inspiration with Daily Journal Dive into a treasure trove of captivating content that spans across diverse topics. From thought-provoking articles on science, technology, and lifestyle, to heartwarming stories of personal growth and resilience, our blog offers something for every curious mind. Explore travel adventures, culinary delights, and artistry that fuels your creativity. Join our thriving community, engage in discussions, and stay informed with our enriching newsletter. Let's embark on a journey of discovery, inspiration, and mutual growth. Welcome to Daily Journal, where curiosity knows no bounds. Happy exploring!";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
-let posts = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", (req,res)=> {
-  res.render("home", {homeContent: homeStartingContent,posts: posts});
-})
-
-app.get("/about", (req,res)=> {
-  res.render("about", {aboutContent: aboutContent});
-})
-
-app.get("/contact", (req,res)=> {
-  res.render("contact", {contactContent: contactContent});
-})
-
-app.get("/compose", (req,res)=> {
-  res.render("compose");
-})
-
-app.post("/compose", (req,res)=> {
-
-  const post = {
-    postTitle: req.body.composeTitle,
-    postBody: req.body.composeBody,
-  };
-
-  posts.push(post);
-
-  res.redirect("/");
-})
-
-app.get("/posts/:postsName", (req,res)=> {
-  posts.forEach( (post)=> {
-    if (_.lowerCase(post.postTitle) === _.lowerCase(req.params.postsName)) {
-      res.render("post", {
-        blogTitle: post.postTitle,
-        blogBody: post.postBody
-      });
-    }
+app.get("/", function(req, res){
+  Post.find({}).then(foundPosts => {
+    res.render("home", {
+    startingContent: homeStartingContent,
+    posts: foundPosts
+    });
   })
-})
+  
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  })
+
+  post.save();
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postId", function(req, res){
+  const requestedId = req.params.postId;
+
+  Post.findOne({_id: requestedId}).then(post=>{
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+  });
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
